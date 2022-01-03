6c10873f167dcd3a4ac8557fbda267bf3f57502f
hunesh <huneshk@gmail.com>
2022-01-03 13:16:44 +0500

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+
+const app = express()
+
+app.use(bodyParser.json({limit:"20mb", extended:true}))
+app.use(bodyParser.urlencoded({limit:"20mb", extended:true}))
+
+app.use(cors())
+
+const CONNECTION_URL="mongodb+srv://hunesh:admin1996@cluster0.qumje.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT=process.env|| 8000;
+mongoose.connect(CONNECTION_URL).then(()=>{
+    console.log('sucessfuly coneected')
+})
+.catch((err)=>{
+    console.log(err.message)
+})
